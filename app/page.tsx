b90f601cc56826e3fb0ec41596829ae15c4a9296
Tanner B. Hess Webber <tb@hesswebber.com>
2023-02-20 14:39:32 +0000

    core: updates home page and global styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
+import Link from "next/link";
+
+import styles from "./home.module.css";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <main>
+      <nav className={styles.primaryNav}>
+        <Link className={styles.navLink + " " + styles.current} href="/home">
+          Home
+        </Link>
+        <Link className={styles.navLink} href="/blog">
+          Blog
+        </Link>
+        <Link className={styles.navLink} href="/contact">
+          Contact me
+        </Link>
+      </nav>
+      <dl>
+        <dt className="visually-hidden">Name</dt>
+        <dd>Tanner B. Hess Webber</dd>
+      </dl>
+    </main>
+  );
 }
